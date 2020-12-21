4b452e6956cbd87e032f17edd989551511db3bd0
Denis Forveille <titou10.titou10@gmail.com>
2020-12-20 21:38:03 -0500

    initial

---
+++ OLWeb/src/main/java/test/TestService.java
@@ -0,0 +1,19 @@
+package test;
+
+import javax.ejb.Stateless;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+import org.eclipse.microprofile.metrics.annotation.SimplyTimed;
+
+@Path("/hello")
+@Stateless
+@SimplyTimed(absolute = true)
+public class TestService {
+
+	@GET
+	public String sayHello() {
+		return "hello";
+	}
+
+}
